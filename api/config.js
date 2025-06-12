b438a52d09571b6c6c93608f4168d4406c50708b
geshanxy <132800727+geshanxy@users.noreply.github.com>
2025-06-12 22:28:46 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://talk.laomchiyu.com:8092/config");
+    const response = await fetch("http://18.163.40.64:8091/config");
     const data = await response.text(); // 或 .json() 视你接口返回内容决定
     res.status(200).send(data);
   } catch (error) {
